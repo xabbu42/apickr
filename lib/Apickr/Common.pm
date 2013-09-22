4f90d87a0e0c904e1c62d580867f798b6ea880b3
Nathan Gass <gass@search.ch>
2013-09-22 17:04:21 +0200

    don't overload --verbose

--- lib/Apickr/Common.pm
+++ lib/Apickr/Common.pm
@@ -19,9 +19,7 @@ sub display {
 
 	my $regex = $main::opts->{select} // '\b\B';
 
-	if ($main::opts->{verbose}) {
-		$regex = '.';
-	} elsif (!$main::opts->{'select-only'} && (!$main::opts->{count} || !$main::opts->{select})) {
+	if (!$main::opts->{'select-only'} && (!$main::opts->{count} || !$main::opts->{select})) {
 		$regex .= '|^\.('
 			. '((flickr\.)?(title|photoset\.title|contexts\.title|info\.people\.list\.(.*)\.username|datetaken|tags|views|sets|exif))' #flickr photos keys
 			. '|photos|count_views|count_comments'                                 #flickr photoset keys
